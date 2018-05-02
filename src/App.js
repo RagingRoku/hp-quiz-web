af65075ac283d52df0e86876f4743f6d40a50524
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-05-02 12:57:53 -0400

    style: add initial style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Question from './components/Question';
-import logo from './logo.svg';
+import hallows from './hallows.png';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Harry Potter | Book 1 Quiz</h1>
+          <img src={hallows} className="App-logo" alt="logo" />
+          <h1 className="App-title">Harry Potter and the Sorcer's Stone Quiz</h1>
         </header>
         <Question content="Where does Mr. Dursley work?" />
       </div>
